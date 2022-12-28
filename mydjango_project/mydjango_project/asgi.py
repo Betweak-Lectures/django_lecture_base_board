abb11968a7b452a899c7f3d48ebeae7840bf1b12
BeTweak <ys@betweak.com>
2022-12-28 09:24:18 +0900

    # Rename Project

--- mydjango_project/mydjango_project/asgi.py
+++ mydjango_project/mydjango_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for mydjango_board project.
+ASGI config for mydjango_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mydjango_board.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mydjango_project.settings')
 
 application = get_asgi_application()
--- mydjango_project/mydjango_project/wsgi.py
+++ mydjango_project/mydjango_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mydjango_board project.
+WSGI config for mydjango_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mydjango_board.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mydjango_project.settings')
 
 application = get_wsgi_application()
