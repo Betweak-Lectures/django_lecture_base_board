9ab0f476230d9352c9a22c88c59e5baa79ea0268
BeTweak <ys@betweak.com>
2022-12-29 21:15:46 +0900

    # Model: Board Model 생성

--- mydjango_project/board/models.py
+++ mydjango_project/board/models.py
@@ -1,3 +1,11 @@
+# board/models.py
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Board(models.Model):
+    id = models.AutoField(primary_key=True)  # integer(auto-increment)
+    title = models.CharField(max_length=255)  # varchar(255)
+    content = models.TextField()  # Text
+    created_at = models.DateTimeField(auto_now_add=True)  # 추가될 때 default로 현재시간
+    updated_at = models.DateTimeField(auto_now=True)  # 추가or업데이트 될 때 default로 현재시간
