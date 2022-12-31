3e850cdf083f33511d358a05eb70a95c5143d243
BeTweak <ys@betweak.com>
2022-12-31 16:13:01 +0900

    # detail page 완성

--- mydjango_project/board/urls.py
+++ mydjango_project/board/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:board_id>', views.board_detail, name='index')
 ]
-
--- mydjango_project/board/views.py
+++ mydjango_project/board/views.py
@@ -8,3 +8,8 @@ from .models import Board, Comment
 def index(request):
     board_list = Board.objects.all()
     return render(request, 'board/index.html', {'board_list': board_list})
+
+
+def board_detail(request, board_id):
+    board = Board.objects.prefetch_related('comment_set').get(id=board_id)
+    return render(request, 'board/detail.html', {'board': board})
