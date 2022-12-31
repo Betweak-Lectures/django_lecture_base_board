6d3a591a68e61061f73a41f464ee49a4a0cebcb1
BeTweak <ys@betweak.com>
2022-12-31 16:27:52 +0900

    # bug삭제

--- mydjango_project/board/urls.py
+++ mydjango_project/board/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'board'
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:board_id>', views.board_detail, name='index')
+    path('<int:board_id>', views.board_detail, name='detail'),
+    path('write', views.board_create, name='write'),
 ]
