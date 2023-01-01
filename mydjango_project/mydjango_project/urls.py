fb46667d21e336f2c4399e3fd1c78651b5e93151
BeTweak <ys@betweak.com>
2023-01-01 14:45:44 +0900

    # static file serving하기까지

--- mydjango_project/mydjango_project/urls.py
+++ mydjango_project/mydjango_project/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('board/', include('board.urls')),
-    path('user/', include('user.urls'))
-]
+from django.conf import settings
+from django.conf.urls.static import static
 
+urlpatterns = [
+                  path('admin/', admin.site.urls),
+                  path('board/', include('board.urls')),
+                  path('user/', include('user.urls')),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
