33e6ceb48c27e58398a14987d278ad7a36dc41c5
BeTweak <ys@betweak.com>
2023-01-01 16:39:49 +0900

    # 회원가입 완료

--- mydjango_project/user/views.py
+++ mydjango_project/user/views.py
@@ -1,15 +1,24 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.urls import reverse
+
+from .forms import RegisterForm
 
 
-# Create your views here.
 def index(request):
-    print(request.user)
-    print(type(request.user))
-
-    print(type(request.session))
-    for k, v in request.session.items():
-        print(k, v)
-    print(dir(request.user))
-
     return render(request, 'user/profile.html')
+
+
+def register(request):
+    form = RegisterForm()
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)  # 로그인
+            return redirect(reverse('user:index'))
+    return render(request, 'user/register.html', {'form': form})
